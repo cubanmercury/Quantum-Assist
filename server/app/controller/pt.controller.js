c4e2b610251b96339cbb1834d465089be752495e
cubanmercury <a_martin15@hotmail.com>
2019-08-14 21:23:08 +0100

    added more fields to /pt endpoint

--- server/app/controller/pt.controller.js
+++ server/app/controller/pt.controller.js
@@ -2,7 +2,7 @@ const db = require('../config/db.config.js');
 const Elements = db.elements;
 
 exports.findAll = (req, res) => {
-    Elements.findAll({attributes: ['atomic_number','symbol','name']})
+    Elements.findAll({attributes: ['atomic_number','symbol','name', 'mass_number', 'electron_configuration','e_group', 'e_period', 'block', 'radioactive']})
     .then(elements => {
         res.status(200).send(elements);
     }).catch(err => {
